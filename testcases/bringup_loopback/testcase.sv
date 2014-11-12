aaf035f72f32f59bc4fa3f2375497bbb8f51e39d
andres-mancera <andres-mancera@users.noreply.github.com>
2014-11-12 15:14:41 -0800

    Increased and randomized the number of packets per test.

--- testcases/bringup_loopback/testcase.sv
+++ testcases/bringup_loopback/testcase.sv
@@ -34,7 +34,7 @@ program testcase(interface tcif);
 
     // Connect packet handle from driver to testcase_packet
     env0.drv.xge_mac_pkt = testcase_packet;
-    num_packets = 10;
+    num_packets = $urandom_range(40,60);
     tcif.reset_dut();
     tcif.init_tb_signals();
     tcif.wait_ns(2000);
