3b7f7517f8f367e4b549930143e4ba5f89ebee6a
andres-mancera <andres-mancera@users.noreply.github.com>
2014-11-21 12:38:41 -0800

    Making some changes to the bringup test case - Adding a 2nd interface for the monitor. - Adding a distribution for the value of EtherType. - Sligthly modifying the packet payload. - Increasing the packet's IPG - Other changes required after monitor integration.

--- testcases/bringup_loopback/testcase.sv
+++ testcases/bringup_loopback/testcase.sv
@@ -1,8 +1,10 @@
 `include "../../testbench/packet.sv"
 `include "../../testbench/driver.sv"
+`include "../../testbench/monitor.sv"
 `include "../../testbench/env.sv"
 
-program testcase(interface tcif);
+program testcase (  interface tcif_driver,
+                    interface tcif_monitor  );
 
   // Since this is a bringup test, all the fields of the
   // packet will be heavily constrained
@@ -15,12 +17,12 @@ program testcase(interface tcif);
       {
         mac_dst_addr    == 48'hAABB_CCDD_EEFF;
         mac_src_addr    == 48'h1122_3344_5566;
-        ether_type      == 16'h88DD;    // IPv6
+        ether_type      dist { 16'h0800:=40, 16'h0806:=20, 16'h88DD:=40 };  // IPv4, ARP, IPv6
         foreach( payload[j] )
         {
-          payload[j]    == j;
+          payload[j]    == j+1;
         }
-        ipg             == 10;
+        ipg             == 100;
       }
   endclass : bringup_packet
 
@@ -29,16 +31,16 @@ program testcase(interface tcif);
   bringup_packet    testcase_packet;
 
   initial begin
-    env0            = new(tcif);
+    env0            = new(tcif_driver, tcif_monitor);
     testcase_packet = new();
 
     // Connect packet handle from driver to testcase_packet
     env0.drv.xge_mac_pkt = testcase_packet;
-    num_packets = $urandom_range(40,60);
-    tcif.init_tb_signals();
-    tcif.wait_ns(2000);
+    num_packets = 5;    //$urandom_range(40,60);
+    tcif_driver.init_tb_signals();
+    tcif_driver.wait_ns(2000);
     env0.run(num_packets);
-    tcif.wait_ns(10000);
+    tcif_driver.wait_ns(10000);
     $finish;
     //#1000 $finish;
   end
