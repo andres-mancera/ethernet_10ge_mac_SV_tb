b6a7e149b24a7b9454ec40635435ca09274c32a5
andres-mancera <andres-mancera@users.noreply.github.com>
2014-11-21 11:44:54 -0800

    Enhancing the print() function so that payload is nicely displayed. Also relaxing the constraint over the packet's IPG.

--- testbench/packet.sv
+++ testbench/packet.sv
@@ -23,7 +23,7 @@ class packet;
   }
 
   constraint C_ipg {
-    ipg inside {[10:20]};
+    ipg inside {[10:1000]};
   }
 
 
@@ -33,13 +33,61 @@ class packet;
 
 
   // ======== Class methods ========
-  function void print();
-    $display("PACKET :: t=%2t, pkt_id=%0d, mac_dst_addr=%h, mac_src_addr=%h, ether_type=%h, payload size=%0d",
-              $time, pkt_id, mac_dst_addr, mac_src_addr, ether_type, payload.size());
-    //for ( int i=0; i<payload.size(); i++ ) begin
-    //  $display("PACKET :: t=%2t, pkt_id=%0d, payload[%0d]=%h", 
-    //            $time, pkt_id, i, payload[i]);
-    //end
+  function void print(string calling_class);
+    int unsigned    Byte8_words;
+    $display("PACKET %s :: t=%2t, pkt_id=%0d, mac_dst_addr=%h, mac_src_addr=%h, ether_type=%h, payload_size=%0d",
+              calling_class, $time, pkt_id, mac_dst_addr, mac_src_addr, ether_type, payload.size());
+    if ( payload.size()>0 ) begin
+      Byte8_words = payload.size()%8 ? payload.size()/8+1 : payload.size()/8;
+      for ( int i=0; i<Byte8_words; i++ ) begin
+        if ( i!=Byte8_words-1 ) begin
+          $display("PACKET %s :: t=%2t, payloadBytes[%0d:%0d]=%h%h%h%h_%h%h%h%h", calling_class, 
+                    $time, 8*i, 8*i+7, payload[8*i], payload[8*i+1], payload[8*i+2], payload[8*i+3],
+                    payload[8*i+4], payload[8*i+5], payload[8*i+6], payload[8*i+7]);
+        end
+        else begin
+          case (payload.size()%8)
+            0: begin
+              $display("PACKET %s :: t=%2t, payloadBytes[%0d:%0d]=%h%h%h%h_%h%h%h%h", calling_class,
+                        $time, 8*i, 8*i+7, payload[8*i], payload[8*i+1], payload[8*i+2], 
+                        payload[8*i+3], payload[8*i+4], payload[8*i+5], payload[8*i+6], 
+                        payload[8*i+7]);
+            end
+            1: begin
+              $display("PACKET %s :: t=%2t, payloadBytes[%0d]=%h", calling_class, $time,
+                        8*i, payload[8*i]);
+            end
+            2: begin
+              $display("PACKET %s :: t=%2t, payloadBytes[%0d:%0d]=%h%h", calling_class, $time,
+                        8*i, 8*i+1, payload[8*i], payload[8*i+1]);
+            end
+            3: begin
+              $display("PACKET %s :: t=%2t, payloadBytes[%0d:%0d]=%h%h%h", calling_class, $time,
+                        8*i, 8*i+2, payload[8*i], payload[8*i+1], payload[8*i+2]);
+            end
+            4: begin
+              $display("PACKET %s :: t=%2t, payloadBytes[%0d:%0d]=%h%h%h%h", calling_class, $time,
+                        8*i, 8*i+3, payload[8*i], payload[8*i+1], payload[8*i+2], payload[8*i+3]);
+            end
+            5: begin
+              $display("PACKET %s :: t=%2t, payloadBytes[%0d:%0d]=%h%h%h%h_%h", calling_class, $time,
+                        8*i, 8*i+4, payload[8*i], payload[8*i+1], payload[8*i+2], payload[8*i+3],
+                        payload[8*i+4]);
+            end
+            6: begin
+              $display("PACKET %s :: t=%2t, payloadBytes[%0d:%0d]=%h%h%h%h_%h%h", calling_class, 
+                        $time, 8*i, 8*i+5, payload[8*i], payload[8*i+1], payload[8*i+2], 
+                        payload[8*i+3], payload[8*i+4], payload[8*i+5]);
+            end
+            7: begin
+              $display("PACKET %s :: t=%2t, payloadBytes[%0d:%0d]=%h%h%h%h_%h%h%h", calling_class, 
+                        $time, 8*i, 8*i+6, payload[8*i], payload[8*i+1], payload[8*i+2], 
+                        payload[8*i+3], payload[8*i+4], payload[8*i+5], payload[8*i+6]);
+            end
+          endcase
+        end
+      end
+    end
   endfunction : print
 
   static function increase_pktid();
