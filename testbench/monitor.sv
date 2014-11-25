301f0968ae47264143705bc196cdc9197a4d534e
andres-mancera <andres-mancera@users.noreply.github.com>
2014-11-25 14:10:46 -0800

    Changes required to handle the missing_eop case gracefully.

--- testbench/monitor.sv
+++ testbench/monitor.sv
@@ -18,10 +18,14 @@ class monitor;
   task collect_packet();
     packet      mon_pkt;
     bit         pkt_in_progress;
+    bit         err_in_packet;
     bit [7:0]   rx_data_q[$];
     int         idx;
+    bit         packet_captured;
     mon_pkt         = new();
     pkt_in_progress = 0;
+    packet_captured = 0;
+    err_in_packet   = 0;
 
     forever begin
       @(mon_vi.cb)
@@ -30,12 +34,19 @@ class monitor;
           mon_vi.cb.pkt_rx_ren <= 1'b1;  
         end
         if ( mon_vi.cb.pkt_rx_val ) begin
-          if ( mon_vi.cb.pkt_rx_sop && pkt_in_progress==0 ) begin
+          if ( mon_vi.cb.pkt_rx_sop && !mon_vi.cb.pkt_rx_eop && pkt_in_progress==0 ) begin
             // -------------------------------- SOP cycle ----------------
             pkt_in_progress = 1;
             mon_vi.cb.pkt_rx_ren <= 1'b1;
+            mon_pkt.sop_mark            = mon_vi.cb.pkt_rx_sop;
             mon_pkt.mac_dst_addr        = mon_vi.cb.pkt_rx_data[63:16];
             mon_pkt.mac_src_addr[47:32] = mon_vi.cb.pkt_rx_data[15:0];
+            mon_pkt.mac_src_addr[31:0]  = 32'h0;
+            mon_pkt.ether_type          = 16'h0;
+            mon_pkt.payload = new[0];
+            while ( rx_data_q.size()>0 ) begin
+              rx_data_q.pop_front();
+            end
           end   // ---------------------------- SOP cycle ----------------
           if ( !mon_vi.cb.pkt_rx_sop && !mon_vi.cb.pkt_rx_eop && pkt_in_progress==1 ) begin
             // -------------------------------- MOP cycle ----------------
@@ -55,7 +66,9 @@ class monitor;
           end   // ---------------------------- MOP cycle ----------------
           if ( mon_vi.cb.pkt_rx_eop && pkt_in_progress==1 ) begin
             // -------------------------------- EOP cycle ----------------
+            mon_pkt.eop_mark= mon_vi.cb.pkt_rx_eop;
             pkt_in_progress = 0;
+            err_in_packet   = mon_vi.cb.pkt_rx_err;
             mon_vi.cb.pkt_rx_ren <= 1'b0;
             if ( mon_vi.cb.pkt_rx_mod==0 ) begin
               for ( int i=0; i<8; i++ ) begin
@@ -77,10 +90,38 @@ class monitor;
               mon_pkt.payload[idx]  = rx_data_q.pop_front();
               idx++;
             end
+            packet_captured  = 1;
+            mon_pkt.rx_error = err_in_packet;
+            //mon_pkt.print("FROM MONITOR");
+            //if ( !err_in_packet ) begin
+            //  // Put the collected packet into the mailbox
+            //  mon2sb.put(mon_pkt);
+            //end
+            // -------------------------------- EOP cycle ----------------
+          end
+          if ( mon_vi.cb.pkt_rx_sop && mon_vi.cb.pkt_rx_eop && pkt_in_progress==0) begin
+            // -------------------------------- SOP/EOP cycle ------------
+            err_in_packet   = mon_vi.cb.pkt_rx_err;
+            mon_vi.cb.pkt_rx_ren <= 1'b1;
+            mon_pkt.sop_mark            = mon_vi.cb.pkt_rx_sop;
+            mon_pkt.eop_mark            = mon_vi.cb.pkt_rx_eop;
+            mon_pkt.mac_dst_addr        = mon_vi.cb.pkt_rx_data[63:16];
+            mon_pkt.mac_src_addr[47:32] = mon_vi.cb.pkt_rx_data[15:0];
+            mon_pkt.mac_src_addr[31:0]  = 32'h0;
+            mon_pkt.ether_type          = 16'h0;
+            mon_pkt.payload = new[0];
+            while ( rx_data_q.size()>0 ) begin
+              rx_data_q.pop_front();
+            end
+            packet_captured = 1;
+            mon_pkt.rx_error = err_in_packet;
+            // -------------------------------- SOP/EOP cycle ------------
+          end
+          if ( packet_captured ) begin
             mon_pkt.print("FROM MONITOR");
             // Put the collected packet into the mailbox
             mon2sb.put(mon_pkt);
-            // -------------------------------- EOP cycle ----------------
+            packet_captured = 0;
           end
         end
       end      
