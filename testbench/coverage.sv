0d3ebbe8149c53cfccdeef83f348e6bf4c5101ea
andres-mancera <andres-mancera@users.noreply.github.com>
2014-12-01 08:17:28 -0800

    Improving the coverpoints by adding some appropriate bins.

--- testbench/coverage.sv
+++ testbench/coverage.sv
@@ -14,23 +14,35 @@ class coverage;
   covergroup cov_packet_cg;
     mac_dst_addr : coverpoint cov_packet.mac_dst_addr
                     {
-                        option.auto_bin_max = 4;
+                        bins ucast_dst_addr = { [48'h0:48'hFFFFFFFFFF] };
+                        bins mcast_dst_addr = { [48'h10000000000:48'hFFFFFFFFFFFE] };
+                        bins bcast_dst_addr = { 48'hFFFFFFFFFFFF };
                     }
     mac_src_addr : coverpoint cov_packet.mac_src_addr
                     {
-                        option.auto_bin_max = 4;
+                        bins ucast_src_addr = { [48'h0:48'hFFFFFFFFFF] };
+                        bins mcast_src_addr = { [48'h10000000000:48'hFFFFFFFFFFFE] };
+                        bins bcast_dst_addr = { 48'hFFFFFFFFFFFF };
                     }
     ether_type   : coverpoint cov_packet.ether_type
                     {
-                        option.auto_bin_max = 4;
+                        bins ipv4   = { 16'h0800 };
+                        bins arp    = { 16'h0806 };
+                        bins ipv6   = { 16'h86DD };
+                        bins fcoe   = { 16'h8906 };
+                        bins others = default;
                     }
     payload      : coverpoint cov_packet.payload.size()
                     {
-                        option.auto_bin_max = 4;
+                        bins undersize_pkt  = { [0:45] };
+                        bins small_pkt      = { [46:256] };
+                        bins medium_pkt     = { [257:1000] };
+                        bins large_pkt      = { [1001:1500] };
+                        bins oversize_pkt   = { [1501:9000] };
                     }
     ipg          : coverpoint cov_packet.ipg
                     {
-                        option.auto_bin_max = 4;
+                        option.auto_bin_max = 8;    // Automatically create 8 bins
                     }
   endgroup
 
